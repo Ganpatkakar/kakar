a85856971810c0ec81243d5d5e154fce0f64c944
Ganpatkakar <kakarganpat@gmail.com>
2017-10-11 08:00:02 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+  
+   $routeProvider
+     .when("/chart", {
+        templateUrl : "chart.html",
+        controller : "chartController"
+    })
+     .when("/calendar", {
+     templateUrl : "calendar.htm"
+   })
+     .when("/blank", {
+     templateUrl : "blank.htm"
+   })
+     .when("/404", {
+     templateUrl : "404.htm"
+   })
+     .when("/login", {
+     templateUrl : "login.htm"
+   })
+     .when("/signup", {
+     templateUrl : "signup.htm"
+   })
+     .when("/profile", {
+     templateUrl : "profile.htm"
+   })
+  .when("/home", {
+     templateUrl : "home.html",
+     controller : "homeController"
+   });
+});
+app.controller("homeController", function($scope){
+  $scope.page_controller = "Home";
+});
+
+app.controller("chartController", function($scope){
+  $scope.pageName = "ChartJs";
+});
